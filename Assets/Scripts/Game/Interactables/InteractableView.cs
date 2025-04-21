9c4f68cf5a599ed1c21257072b09373f4830e8cd
Rehsat <alesandrgorshkov@mail.ru>
2025-04-21 20:39:38 +0300

    added filter on outline select for interactebles

--- Assets/Scripts/Game/Interactables/InteractableView.cs
+++ Assets/Scripts/Game/Interactables/InteractableView.cs
@@ -61,6 +61,7 @@ namespace Game.Interactables
         }
         public void SetIsSelectedState(bool isSelected)
         {
+            isSelected = isSelected && _actions[Interaction.InteractButton].Count > 0;
             _outline.OutlineColor = isSelected ? Color.white : _unselectedColor;
             _outline.OutlineWidth = isSelected ? _startOutlineWidth * 3 : _startOutlineWidth;
         }
